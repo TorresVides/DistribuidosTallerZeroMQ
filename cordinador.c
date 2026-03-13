df6942f2a846e8834765b26390e1f3dbcaa47528
unknown <mateod.guerram.war@gmail.com>
2026-03-12 19:49:26 -0500

    Request/Reply model

--- cordinador.c
+++ cordinador.c
@@ -1,42 +1,20 @@
-###############################################################################################
-#	cordinador.py -> Da permiso de entrar y salir de la region critica
-#
-#
-################################################################################################
+#include <zmq.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
 
-include <zmq.h>
-include <stdio.h>
-include <unistd.h>
-include <string.h>
-include <stdlib.h>
-include <time.h>
+int main (void) {
+    //  Socket to talk to clients
+    void *context = zmq_ctx_new ();
+    void *responder = zmq_socket (context, ZMQ_REP);
+    int rc = zmq_bind (responder, "tcp://*:5555");
+    assert (rc == 0);
 
-int main (void) {  // programa publisher.c
-    void *context = zmq_ctx_new();
-    void *publisher = zmq_socket(context, ZMQ_PUB);
-    
-    // Bind al puerto 5556
-    int rc = zmq_bind(publisher, "tcp://*:5556");
-    if (rc != 0) { perror("Error bind"); return 1; }
-
-    srand(time(NULL));
-    printf("Publicador enviando DEPORTES y CINE...\n");
-    int max = 1;
-    while (max <  10) {
-        char message[100];
-        int score = rand() % 10; // Tópico 1: DEPORTES
-        sprintf(message, "DEPORTES Marcador final: %d", score);
-        zmq_send(publisher, message, strlen(message), 0);
-        
-        int rating = (rand() % 5) + 1; // Tópico 2: CINE
-        sprintf(message, "CINE Nueva película, rating: %d estrellas", rating);
-        zmq_send(publisher, message, strlen(message), 0);
-
-        usleep(500000); // Enviar cada 0.5 segundos
-        max++;
+    while (1) {
+         char buffer [2];
+         zmq_recv (responder, buffer, 2, 0);
+         printf ("Received: %s\n",buffer);
     }
-    //  We never get here, but clean up anyhow
-    zmq_close(publisher);
-    zmq_ctx_destroy(context);
     return 0;
 }
--- worker01.c
+++ worker01.c
@@ -1,5 +1,16 @@
-################################################################################################
-#
-#	worker01.py -> Le pide enter() al cordinador antes que worker02
-#
-################################################################################################
+// ZMQ Client (Requester)
+// Initialize context, create ZMQ_REQ socket, and connect
+void *context = zmq_ctx_new();
+void *requester = zmq_socket(context, ZMQ_REQ);
+zmq_connect(requester, "tcp://localhost:5555");
+
+for (int n = 0; n < 10; n++) {
+    char buffer[10];
+    // Send request then wait for reply
+    zmq_send(requester, "Hello", 5, 0);
+    zmq_recv(requester, buffer, 10, 0);
+    printf("Received: %s\n", buffer);
+}
+// Clean up
+zmq_close(requester);
+zmq_ctx_destroy(context);
